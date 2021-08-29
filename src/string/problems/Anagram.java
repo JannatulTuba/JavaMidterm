4663c29b9c9c8b1948cc7bc6bfec0fece258c7ab
= <=>
2021-08-29 08:16:24 +0600

    anagram problem solved

--- src/string/problems/Anagram.java
+++ src/string/problems/Anagram.java
@@ -1,5 +1,8 @@
 package string.problems;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Anagram {
 
     /*
@@ -9,6 +12,40 @@ public class Anagram {
     */
 
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter 1st  string : ");
+        String word1 = sc.next();
+        System.out.println("Enter 2nd  string : ");
+        String word2 = sc.next();
+
+        char[] ch1 = word1.toCharArray();// converting string to characters array
+        char[] ch2 = word2.toCharArray();// converting string to characters array
+
+
+        Arrays.sort(ch1); // sorting characters array
+        Arrays.sort(ch2); // sorting characters array
+
+        boolean isAnagram = true;
+
+        if(word1.length() == word2.length()){
+            for(int i =0; i< word1.length(); i++){
+                if(ch1[i] != ch2[i]){
+                    isAnagram = false;
+                    break;
+                }
+            }
+            if(isAnagram){
+                System.out.println("Anagram");
+            }else {
+                System.out.println(" Not Anagram");
+            }
+        }else {
+            System.out.println("Not anagram");
+        }
+
+
+
 
     }
 }
