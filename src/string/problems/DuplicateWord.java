61f97cb0111bb60a341cd720f91cf7f53dc996d9
= <=>
2021-08-30 07:18:40 +0600

    Half Completed

--- src/string/problems/DuplicateWord.java
+++ src/string/problems/DuplicateWord.java
@@ -1,5 +1,8 @@
 package string.problems;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class DuplicateWord {
 
     public static void main(String[] args) {
@@ -10,7 +13,35 @@ public class DuplicateWord {
          */
 
         String st = "Java is a programming Language. Java is also an Island of Indonesia. Java is widely used language";
+        char c = ' ';
+        String word = " ";
+        int j = 1;
 
+        Map<Integer, String> map = new HashMap<Integer, String>();
+        Map<String, Integer> duplicates = new HashMap<String, Integer>();
+
+        for (int i = 0; i < st.length(); i++){
+             c = st.charAt(i);
+             if( c != ' '){
+                 word = word + c;
+             }
+             else {
+                 map.put(j,word);
+                 word = " ";
+                 j++;
+             }
+
+                 }
+//        for (Integer i : map.keySet()) {
+
+//
+//
+//
+//
+//        }
+
+
+        System.out.println(map);
     }
 
 }
