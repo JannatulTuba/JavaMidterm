1f65fbd8ab15cd57e991ccced656b177db35a3f8
= <=>
2021-08-31 23:45:01 +0600

    permutation solved

--- src/string/problems/Permutation.java
+++ src/string/problems/Permutation.java
@@ -1,6 +1,36 @@
 package string.problems;
 
+import java.util.Scanner;
+
 public class Permutation {
+
+    //  print all the permutations of given string
+    static void PrintStringPermutation(String str,    String result )
+    {
+
+        // check if string is empty or not
+        if (str.length() == 0) {
+            System.out.print(result + " ");
+            return;
+        }
+
+        for (int i = 0; i < str.length(); i++) {
+
+            // ith character of str
+            char ch = str.charAt(i);
+
+            // Rest of the string after excluding the ith character
+             String ros = str.substring(0, i) + str.substring(i + 1);
+
+//            System.out.println(str.substring(i + 1));//b when i =0
+//            System.out.println(str.substring(0, i));//c when i = 0
+//            System.out.println(ros);//bc when i =0
+
+            // Recurvise call
+            PrintStringPermutation(ros, result + ch);
+        }
+    }
+
 
     public static void main(String[] args) {
 
@@ -9,5 +39,14 @@ public class Permutation {
 
         Write Java program to compute all permutations of any given String
          */
+
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a string: ");
+        String word = sc.next();
+        PrintStringPermutation(word," ");
     }
+
+
+
 }
