8780c3022bbebd5d8c8bd8018a2c9f99bf4c1cdf
= <=>
2021-08-31 21:39:53 +0600

    UseArrayList Solved

--- src/datastructure/UseArrayList.java
+++ src/datastructure/UseArrayList.java
@@ -1,6 +1,10 @@
 package datastructure;
 
+import java.util.ArrayList;
+
 public class UseArrayList {
+
+
 
     public static void main(String[] args) {
         /*
@@ -9,6 +13,40 @@ public class UseArrayList {
          *
          * Store and retrieve data from/to a database table.
          */
+
+        ArrayList<String> student = new ArrayList<String>();
+
+        //add elements in ArrayList
+        student.add("Jhon");
+        student.add("Deep");
+        student.add("Saji");
+        student.add("Arisha");
+        student.add("Mim");
+        System.out.println("Result Before removing :"  + student);
+
+
+        //remove method
+        student.remove( "Deep");
+        System.out.println("Result after removing one element: "+student);
+
+        //get method
+        System.out.println("get method: "+ student.get(2));
+
+        //for each loop
+        System.out.println("***********For each loop result************");
+        for(String name:student){
+            System.out.println(name);
+
+        }
+
+        // while loop
+        System.out.println("***********while loop result***************");
+        int  len = student.size()-1;
+        int i= 0;
+        while (i<=len){
+            System.out.println(student.get(i));
+            i++;
+        }
 
     }
 
