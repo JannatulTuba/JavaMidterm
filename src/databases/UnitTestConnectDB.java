0786a9a3b4be6ff71b925f18abd64c1deb36b371
= <=>
2021-09-03 19:23:08 +0600

    Trying

--- src/databases/UnitTestConnectDB.java
+++ src/databases/UnitTestConnectDB.java
@@ -4,6 +4,8 @@ public class UnitTestConnectDB {
 
     public static void main(String[] args) {
         ConnectToSqlDB connectToSqlDB = new ConnectToSqlDB();
+        User user1 = new User("Tuba","01");
+
 
     }
 }
